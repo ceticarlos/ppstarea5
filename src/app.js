af2f7ea50c45245203d3afef685380adbafdfa21
ceticarlos <ceticarlos1@gmail.com>
2026-04-29 15:59:32 -0400

    fix security vulnerabilities: add helmet headers

--- src/app.js
+++ src/app.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const helmet = require('helmet');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(helmet());
+app.use(helmet.frameguard({ action: 'deny' }));
+app.use(helmet.noSniff());
+app.use(helmet.hidePoweredBy());
+app.use(helmet.contentSecurityPolicy());
 
 // "Base de datos" en memoria
 const tickets = [
